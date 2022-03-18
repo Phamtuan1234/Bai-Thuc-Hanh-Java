e02f746fbf8d1705f90233c136e0627f28509e50
Phamtuan1234 <phamminhtuanhp0123@gmail.com>
2022-03-18 21:34:00 +0700

    baithuchanh1803

--- bai thuc hanh 1803/App.java
+++ bai thuc hanh 1803/App.java
@@ -1,45 +1,21 @@
-import java.util.Scanner;
+import Main.HinhChuNhat;
+import Main.HinhTron;
 
-class DientichchuviHT {
-    public static double tinhDienTich(double r) {
-        return r * r * 3.14;
-    }
-
-    public static double tinhChuVi(double r) {
-        return r * 2 * 3.14;
-    }
-
+public class App {
     public static void main(String[] args) {
-
-    }
-
-    class HinhTron {
-        final float PI = 3.14f;
-        float r;
-        float cv;
-        float dt;
-
-        void nhapBanKinh() {
-            System.out.println("Hay nhap vao ban kinh hinh tron: ");
-            Scanner scanner = new Scanner(System.in);
-            r = scanner.nextFloat();
-        }
-
-        void tinhChuVi() {
-            cv = 2 + PI * r;
-        }
-
-        void tinhDienTich() {
-            dt = PI * r;
-        }
-
-        void inChuVi() {
-            System.out.println("Chu vi hinh tron: " + cv);
-        }
-
-        void inDienTich() {
-            System.out.println("Dien tich hinh tron: " + dt);
-            ;
-        }
-    }
+   HinhTron hinhTron = new HinhTron();
+   HinhChuNhat hinhChuNhat = new HinhChuNhat();
+   hinhTron.nhapbankinh();
+   hinhTron.tinhchuvi();
+   hinhTron.tinhdientich();
+   hinhTron.inchuvi();
+   hinhTron.indientich();
+   System.out.println("\n\n");
+   hinhChuNhat.nhapchieudai();
+   hinhChuNhat.nhapchieurong();
+   hinhChuNhat.tinhchuvi();
+   hinhChuNhat.tinhdientich();
+   hinhChuNhat.inchuvi();
+   hinhChuNhat.indientich();
+   }
 }
