f37408fb8aa82804b56108eb7140bdcdad1354a1
Phamtuan1234 <phamminhtuanhp0123@gmail.com>
2022-04-30 19:45:28 +0700

    commit note

---
+++ Bai Thuc Hanh 2904/src/App2.java
@@ -0,0 +1,10 @@
+import Shape.Quadrilateral;
+
+public class App2 {
+    public static void main(String[] args) throws Exception {
+        Quadrilateral b = new Quadrilateral();
+        b.nhapTuGiac();
+        b.kiemTraTuGiac();
+        b.kiemTraLoaiTuGiac();
+    }
+}
